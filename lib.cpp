66832754b8d68d1edecefda885dc26ea787aeebb
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-12-01 22:19:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+#include "funzione.h"
 
+bool verifica (int a){
+    int i = 2;
+    bool flag = false;
+    if (a!=2){
+        while (i!=a) {
+            if (a%i==0){
+                break;
+            }
+            i++;
+            if (i==a) {
+                flag = true;
+            }
+        }
+    } else {
+        flag = true;
+    }
+    return flag;
+}
