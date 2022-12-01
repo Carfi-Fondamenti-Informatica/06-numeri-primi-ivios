865172bcf6574b36f62f5a9a340cdbe8e009271c
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-12-01 22:19:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "funzione.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int n;
+    cin >> n;
+    if (verifica (n)){
+        cout << "numero primo";
+    } else {
+        cout << "numero non primo";
+    }
+    return 0;
 }
